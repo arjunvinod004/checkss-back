d347e463fed1fa3fef22d189e998f58a732383e5
Coinone Global Solutions Pvt Ltd <arjunvt004@gmail.com>
2024-05-02 10:45:14 +0530

    checkss back

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const cors= require('cors');
 const app = express();
-const port = process.env.Port || 8000
+const port = process.env.PORT || 8000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,6 +38,6 @@ fetch('https://coinoneglobal.in/TERESA_PRD/webapplog.asmx/FnGetUserLogIn?PrmUser
 
 
 app.listen(8000,function(){
-console.log(`${port}`);
+console.log(`connectec to ${port}`);
 })
 
